4d05c95bf47d2566ff43a155c6e6e1c187561520
David Scott <dave@recoil.org>
2016-05-03 10:06:26 -0700

    Add a skeleton of an Lwt subpackage
    Signed-off-by: David Scott <dave@recoil.org>

---
+++ lwt/named_pipe_lwt.ml
@@ -0,0 +1,33 @@
+
+module Server = struct
+  type t = Unix.file_descr
+  let to_fd x = x
+
+  external create: string -> t = "stub_named_pipe_create"
+
+  let connect _ = Lwt.fail (Failure "not implemented")
+
+  let flush _ = Lwt.fail (Failure "not implemented")
+
+  external disconnect: t -> unit = "stub_named_pipe_disconnect"
+
+  external destroy: t -> unit = "stub_named_pipe_destroy"
+end
+
+module Client = struct
+  type t = Unix.file_descr
+  let to_fd x = x
+
+  exception Pipe_busy
+
+  (* TODO: if this fails with ERROR_PIPE_BUSY then call wait *)
+  let openpipe path =
+    try
+      Unix.openfile path [ Unix.O_RDWR ] 0
+    with Unix.Unix_error(Unix.EUNKNOWNERR -231, _, _) ->
+      (* ERROR_PIPE_BUSY *)
+      raise Pipe_busy
+    | e -> raise e
+
+  let wait _ _ = Lwt.fail (Failure "not implemented")
+end
---
+++ lwt/named_pipe_lwt.mli
@@ -0,0 +1,50 @@
+
+(** https://msdn.microsoft.com/en-us/library/windows/desktop/aa365588(v=vs.85).aspx *)
+
+module Server: sig
+  type t
+  (** A single named pipe capable of accepting one connection. *)
+
+  val create: string -> t
+  (** The server should create a named pipe at a particular path under \\.\pipe *)
+
+  val connect: t -> bool Lwt.t
+  (** Connect blocks until a client connects to this named pipe *)
+
+  val to_fd: t -> Unix.file_descr
+
+  val flush: t -> unit Lwt.t
+  (** Flushes outstanding write buffers, typically called before disconnect *)
+
+  val disconnect: t -> unit
+  (** Disconnects the connected client *)
+
+  val destroy: t -> unit
+  (** Removes the underlying OS resource *)
+end
+
+(* https://msdn.microsoft.com/en-gb/library/windows/desktop/aa365592(v=vs.85).aspx *)
+
+module Client: sig
+  type t
+  (** A connection to a named pipe server *)
+
+  exception Pipe_busy
+  (** A pipe is busy if the server is not ready to accept more connections:
+      perhaps it is still forking a thread for the previous one. A client
+      should call the wait function to wait for a free slot. *)
+
+  val openpipe: string -> t
+  (** Connect to the named pipe server on the given path (e.g. \\.\pipe\foo).
+      If the server isn't running then this raises Unix_error(Unix.ENOENT...).
+      If the server isn't ready to accept a connection then this raises
+      Pipe_busy, and the client should call [wait]. *)
+
+  val to_fd: t -> Unix.file_descr
+
+  val wait: string -> int -> bool Lwt.t
+  (** [wait path ms] wait for up to [ms] milliseconds for the server to become
+      available. Returns true if the server has a free slot: in this case
+      the client should call [openpipe] again. Returns false if the server
+      has shutdown. *)
+end

d52b59a54e91a7422027dccbc8ab00987e9bcbdc
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-11-08 20:54:27 +0000

    Add some debug message to the ocamlbuild plugin

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -3,9 +3,14 @@ open Ocamlbuild_plugin
 let incl pkg =
   run_and_read ("opam config var " ^ pkg ^ ":lib")
   |> String.trim
-  |> fun d -> "-I " ^ d
+  |> Printf.sprintf "-I '%s'"
+
+let incl_lwt =
+  let s = incl "lwt" in
+  Printf.printf "Adding cclib(%s)\n%!" s;
+  s
 
 let () = Ocb_stubblr.dispatchv [
-    Ocb_stubblr.cclib (incl "lwt");
+    Ocb_stubblr.cclib incl_lwt;
     Ocb_stubblr.init;
   ]
